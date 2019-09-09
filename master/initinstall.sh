0cbac08592af3fd0f14620eb75d34402cdf556bb
Aiki Sakamoto <sakamoto@qzc.co.jp>
2019-09-09 11:09:41 +0900

    Add initinstall.sh Cygwinの初期インストールシェルスクリプトを作成、追加

---
+++ master/initinstall.sh
@@ -0,0 +1,18 @@
+#/bin/sh
+apt-cyg install wget
+apt-cyg install curl
+apt-cyg install ssh
+apt-cyg install screen
+apt-cyg install whois
+apt-cyg install libiconv
+apt-cyg install zip
+apt-cyg install unzip
+apt-cyg install vim
+apt-cyg install rdiff-backup
+apt-cyg install rsync
+apt-cyg install git
+apt-cyg install tig
+apt-cyg install subversion
+apt-cyg install bc
+apt-cyg install chere
+apt-cyg install bash-completion

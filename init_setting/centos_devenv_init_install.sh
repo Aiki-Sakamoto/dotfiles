15966a42f8ac8104ba9156cee4c34441b3764ac1
Aiki-Sakamoto <sakamoto.a1234@gmail.com>
2019-11-02 16:13:32 +0900

    Update init_settigs CygwinとVagrantのCentOSの初期設定

---
+++ init_setting/centos_devenv_init_install.sh
@@ -0,0 +1,28 @@
+#/bin/sh
+
+# develop env tools
+sudo yum install -y wget
+sudo yum install -y curl
+sudo yum install -y ssh
+sudo yum install -y screen
+sudo yum install -y whois
+sudo yum install -y libiconv
+sudo yum install -y zip
+sudo yum install -y unzip
+sudo yum install -y vim
+sudo yum install -y rdiff-backup
+sudo yum install -y rsync
+sudo yum install -y git-all
+sudo yum install -y tig
+sudo yum install -y subversion
+sudo yum install -y bc
+sudo yum install -y chere
+sudo yum install -y bash-completion
+
+# develop tools for ruby on rails app dependency
+sudo yum install -y gcc
+sudo yum install -y gcc-c++
+sudo yum install -y openssl-devel
+sudo yum install -y readline-devel
+sudo yum install -y zlib-devel
+sudo yum install -y sqlite

f39e82ba18905f3fdad4ff5e6f82a6e69cdf701d
Aiki-Sakamoto <sakamoto.a1234@gmail.com>
2020-05-17 21:50:02 +0900

    Add homebrew_init_install.sh homebrewで一括インストールするシェルスクリプト追加

---
+++ init_setting/homebrew_init_install.sh
@@ -0,0 +1,16 @@
+#/bin/sh
+brew install wget
+brew install curl
+brew install ssh
+brew install screen
+brew install whois
+brew install libiconv
+brew install zip
+brew install unzip
+brew install vim
+brew install rdiff-backup
+brew install rsync
+brew install git
+brew install tig
+brew install bc
+brew install bash-completion

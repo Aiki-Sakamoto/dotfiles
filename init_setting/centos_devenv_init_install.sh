e8a4903976aaf870589c514e712f2e6a9fd85238
sakamoto <sakamoto@qzc.co.jp>
2021-03-31 15:51:51 +0900

    Modify centos_devenv_init_install.sh 内容修正

--- init_setting/centos_devenv_init_install.sh
+++ init_setting/centos_devenv_init_install.sh
@@ -9,8 +9,9 @@ sudo yum update -y
 # install IIS repo and Git newer version
 # sudo yum install -y https://centos7.iuscommunity.org/ius-release.rpm # 2020/06現在使えない
 # そのため、こちらのリポジトリを有効化
-sudo yum -y install https://packages.endpoint.com/rhel/7/os/x86_64/endpoint-repo-1.7-1.x86_64.rpm
-sudo yum -y remove git git-\*
+#sudo yum -y install https://packages.endpoint.com/rhel/7/os/x86_64/endpoint-repo-1.7-1.x86_64.rpm
+#sudo yum -y remove git git-\*
+# IISリポジトリを使わない。2021/03/31 fix
 sudo yum install -y git
 
 # tigをインストールするために有効化 # 2020/06現在
@@ -18,7 +19,7 @@ sudo yum -y install epel-release.noarch
 sudo yum -y install tig
 
 # develop env tools
-sudo yum install -y wget curl screen whois zip unzip vim rsync subversion bc bash-completion
+sudo yum install -y wget curl screen whois zip unzip vim rsync bc bash-completion
 
 # develop tools for ruby on rails app dependency
 sudo yum install -y gcc gcc-c++ openssl-devel readline-devel zlib-devel sqlite-devel postgresql-devel node.js npm mysql mysql-devel
